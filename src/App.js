c1b91a564f0c6f2138cc183b92ac1831ea295458
Sanidhya Kumar Rawat <sanidhyarawat200@gmail.com>
2023-02-17 21:27:18 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import {useSelector}  from "react-redux";
+// import {useSelector}  from "react-redux";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
-import  selectUser from "./features/userSlice";
-import Login from "./Login";
+// import  selectUser from "./features/userSlice";
+// import Login from "./Login";
 
 function App() {
   // const user = useSelector(selectUser);
   return (
     <div className="app">
-      <Login />
+          <>
+          <Sidebar />
+          <Chat />
+          </>
       {/* { 
         
         user ? (
